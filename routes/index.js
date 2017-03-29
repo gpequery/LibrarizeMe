310cfc61bc33f538b25e734e760407134117d459
Your Name <you@example.com>
2017-03-29 16:02:05 +0200

    Racine

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-  res.send('COUCOU');
+router.all('/', function(req, res, next) {
+  res.redirect('/user/login');
 });
 
 
